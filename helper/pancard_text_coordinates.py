75ff99be96d3b136c9749d2706fc91113d767c16
Devopscasting <devopcasting@gmail.com>
2024-01-11 21:27:17 +0530

    updated logic

---
+++ helper/pancard_text_coordinates.py
@@ -0,0 +1,22 @@
+import pytesseract
+import re
+
+class TextCoordinates:
+    def __init__(self, image_path, doc_type=None) -> None:
+        self.image_path = image_path
+        self.doc_type = doc_type
+    
+    # func: generate coordinates
+    def generate_text_coordinates(self) -> list:
+        tesseract_config = r'-l eng --oem 3 --psm 11'
+        data = pytesseract.image_to_data(self.image_path, output_type=pytesseract.Output.DICT)
+        special_characters = r'[!@#$%^&*()_\-+{}\[\]:\';<>,.?~\\|]'
+        coordinates = []
+
+        for i in range(len(data['text'])):
+            text = data['text'][i]
+            x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+            # Filter out empty strings and  special characters
+            if text.strip() != '':
+                coordinates.append((x,y,x + w, y + h, text))
+        return coordinates
